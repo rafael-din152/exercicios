1d4dc9f480a1589eae9ee2d18e2ec726db9f649c
Rafael Diniz <rafaelddniz@gmail.com>
2020-06-30 17:19:41 -0300

    Add files via upload

---
+++ Homework1/hw4.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(provide (all-defined-out)) ;; so we can put tests in a second file
+
+;; put your code below
+
+;; Problem 1
+(define (sequence low high stride)
+  (if (> low high)
+      null
+      (cons low (sequence (+ low stride) high stride))))
+
+
+(define (string-append-map xs suffix)
+  (map string-append (map car xs) suffix))
